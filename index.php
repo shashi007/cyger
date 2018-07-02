293e76dd7d6ddd97fcf751c9c7a3829ba5ecb12f
shashi007 <jai.broly@gmail.com>
2018-07-02 17:53:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,21 @@ require 'vendor/autoload.php';
 use Cyger\Client;
 
 $client = new Client();
-$result = $client->setExchange('Poloniex')->setPair('BTC_ETH')->getLastPrice();
-echo $result['Poloniex']['BTC_ETH'];    // 0.04549105
+$result = $client->setExchange('Binance')->setPair('BTC_ETH')->getLastPrice();
+echo $result['Binance']['BTC_ETH'];  
+$result = $client->setExchange('Binance')->setPair('BTC_WAVES')->getLastPrice();
+echo $result['Binance']['BTC_ETH']; 
+$result = $client->setExchange('Binance')->setPair('BTC_KMD')->getLastPrice();
+echo $result['Binance']['BTC_ETH']; 
+$result = $client->setExchange('Binance')->setPair('BTC_AION')->getLastPrice();
+echo $result['Binance']['BTC_ETH']; 
+$result = $client->setExchange('Binance')->setPair('BTC_QSP')->getLastPrice();
+echo $result['Binance']['BTC_ETH']; 
+$result = $client->setExchange('Binance')->setPair('BTC_REQ')->getLastPrice();
+echo $result['Binance']['BTC_ETH']; 
+$result = $client->setExchange('Binance')->setPair('BTC_SNM')->getLastPrice();
+echo $result['Binance']['BTC_ETH']; 
+$result = $client->setExchange('Binance')->setPair('BTC_USD')->getLastPrice();
+echo $result['Binance']['BTC_ETH']; 
 
 ?>
